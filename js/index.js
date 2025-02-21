f23ac65015e55a94a45b2439384855b8c60e5558
LiberiBg <matheo.ngn@gmail.com>
2025-02-21 19:22:49 +0100

    feature(now-page): + refactor and optimizations

--- js/index.js
+++ js/index.js
@@ -4,3 +4,8 @@ fetch('header.html')
 fetch('footer.html')
     .then(response=> response.text())
     .then(text=> document.getElementById('footer-id').innerHTML = text);
+fetch('head.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('head-id').outerHTML = data;
+    });
